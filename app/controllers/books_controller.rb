92d187bfab1f25d7361e43c9e36bc477ec5bb475
pmojb <pmojb_saya1208@yahoo.co.jp>
2021-01-17 09:20:08 +0000

    [finish]8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "successfully"
+    redirect_to books_path
+  end
+
+def create
+    @book = Book.new(book_params)
+    @books = Book.all
+  if @book.save
+    flash[:notice] = "successfully"
+    redirect_to book_path(@book)
+  else
+    render action: :index
+  end
+end
+
+  def update
+    @book = Book.find(params[:id])
+    @books = Book.all
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book)
+    else
+      render action: :index
+    end
+  end
+
+  private
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+end
